1b46ffa5c417a665f0616cc378940ad36d8e1a07
L <ll6209@rit.edu>
2024-04-15 05:12:29 -0400

    making project section responsive

--- src/App.js
+++ src/App.js
@@ -5,21 +5,9 @@ import React from 'react';
 function App() {
   const [progress, setProgress] = React.useState(0);
   
-  React.useEffect(() => {
-    const timer = setInterval(() => 
-  {
-    setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
-  }, 800);
-
-  return () => {
-    clearInterval(timer);
-  }
-  },[])
-  console.log(progress)
-
   return (
     <div>
-       <LandingPage></LandingPage>
+       {/* <LandingPage></LandingPage> */}
        <ProjectsSection value = {progress}></ProjectsSection>
     </div>
   );
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import '../css/ProgressBar.css'
-
+import '../css/ProjectSection.css'
+import Grid from '@mui/material/Grid'
 import CardItem from './CardItem';
 import Carousel from 'react-material-ui-carousel'
 import ProgressBar from './ProgressBar';
+import { Button, IconButton } from '@mui/material';
 
 export default class ProjectsSection extends Component {
   render() {
@@ -17,18 +18,44 @@ export default class ProjectsSection extends Component {
         ["🍆"],
         ["🍇"]
     ]
-
+    const buttonStyle =
+    {
+      fontSize : '3em',
+      height : '40px',
+      width : '40px',
+    }
+    const groupedItems = [];
+    for (let i = 0; i < projectItems.length; i += 3) {
+      groupedItems.push(projectItems.slice(i, i + 3));
+    }
     return (
       <div>
         <h3>
           Projects
         </h3>
         <ProgressBar></ProgressBar>
-        <Carousel>
-          {projectItems.map((img) =>(<CardItem key = {img} img = {img}></CardItem>))}
-        </Carousel>
-      </div>
-      
+
+        <div className='carousel-wrapper'>
+
+            <div className="carousel-container">
+              <Carousel indicators = {false}
+              stopAutoPlayOnHover={true} autoPlay={true}
+              navButtonsAlwaysVisible={true}
+              >
+              {groupedItems.map((group) =>(
+                  <Grid container key={group[0]}>
+                    {group.map(img => (
+                      <Grid item xs={4} lg={4} md={4}key={img}>
+                        <CardItem img={img}></CardItem>
+                      </Grid>
+                    ))}
+                  </Grid>
+              ))}
+              </Carousel>
+            </div>
+        </div>
+              
+       </div>
     )
   }
 }
