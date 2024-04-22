1bc2f74ee15684ea9edbda782d47d7f7b10456e6
L <ll6209@rit.edu>
2024-04-22 04:31:57 -0400

    added cart and sunflower seeds

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,34 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "../css/LoadingScreen.css";
 
 export default function LoadingScreen() {
+  const [cartDirection, setCartDirection] = useState("");
   
-useEffect(() => {
+  useEffect(() => {
     var wheelAndHamster = document.querySelector('.wheel-and-hamster');
-    var direction = Math.floor(Math.random() * 4);
+    var direction = Math.floor(Math.random() * 2); // Generating a random number between 0 and 1
+    var newCartDirection = "";
     switch (direction) {
-        case 0:
-            wheelAndHamster.style.animationName = 'moveRight';
-            break;
-        case 1:
-            wheelAndHamster.style.animationName = 'moveLeft';
-            break;
-        case 2:
-            wheelAndHamster.style.animationName = 'moveUp';
-            break;
-        case 3:
-            wheelAndHamster.style.animationName = 'moveDown';
-            break;
-        default:
-            wheelAndHamster.style.animationName = 'moveRight';
-            break;
+      case 0:
+        newCartDirection = "https://imgur.com/Rwuu8f1.jpeg";
+        wheelAndHamster.style.animationName = 'moveRight';
+        break;
+      case 1:
+        newCartDirection = "https://imgur.com/Rwuu8f1.jpeg";
+        wheelAndHamster.style.animationName = 'moveLeft';
+        break;
+      default:
+        wheelAndHamster.style.animationName = 'moveRight';
+        break;
     }
-}, []);
+    setCartDirection(newCartDirection); // Set the cartDirection state
+  }, []);
+
 
   return (
-    <div>
+    <div className='screen'>
       <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
         <div className="wheel"></div>
+        <div className="container">
+
+          <img className="cart" src={cartDirection} alt=''></img>
+          <div className="hamster"></div>
+          <img className="sunflower-seed" src={"https://imgur.com/NXC2n1f.jpeg"} style={{ width: "100px" }} alt=''></img>
+        
+        </div>
+
         <div className="hamster">
           <div className="hamster__body">
             <div className="hamster__head">
@@ -43,8 +51,10 @@ useEffect(() => {
             <div className="hamster__tail"></div>
           </div>
         </div>
+
         <div className="spoke"></div>
       </div>
+
     </div>
   );
 }
